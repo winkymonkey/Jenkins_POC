9c33298972623fe7de125b3f5c5f3e071dd5d834
apal5 <apal5@intuit.com>
2020-05-16 17:21:36 +0530

    code change

--- src/main/java/com/example/MySpringBootApplication.java
+++ src/main/java/com/example/MySpringBootApplication.java
@@ -7,8 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySpringBootApplication {
+	private static final String token = "FreshWinds";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringBootApplication.class, args);
 		System.out.println("mySecretKey is "+System.getProperty("mySecretKey"));
+		
+		if(token.equals(System.getProperty("mySecretKey")))
+			System.out.println("valid secret");
+		else
+			System.out.println("invalid secret");
 	}
 }
