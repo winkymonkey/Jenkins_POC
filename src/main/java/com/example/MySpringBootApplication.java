9be91f4e2bb9984a9c36a07ba62310973ba300d2
apal5 <apal5@intuit.com>
2020-05-16 16:21:47 +0530

    code change

---
+++ src/main/java/com/example/MySpringBootApplication.java
@@ -0,0 +1,14 @@
+
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MySpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+		System.out.println(System.getProperty("myKey"));
+	}
+}
