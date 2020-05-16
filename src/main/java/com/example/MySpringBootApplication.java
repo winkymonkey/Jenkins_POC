64e06cd4df8b53993e2f2938fc46c89193062c5b
apal5 <apal5@intuit.com>
2020-05-16 18:43:52 +0530

    refactor

--- src/main/java/com/example/MySpringBootApplication.java
+++ src/main/java/com/example/MySpringBootApplication.java
@@ -1,8 +1,8 @@
-
 package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
 
 @SpringBootApplication
@@ -13,7 +13,9 @@ public class MySpringBootApplication {
 		SpringApplication.run(MySpringBootApplication.class, args);
 		System.out.println("mySecretKey is "+System.getProperty("mySecretKey"));
 		
-		if(token.equals(System.getProperty("mySecretKey")))
+		if(StringUtils.isEmpty(System.getProperty("mySecretKey")))
+			System.out.println("secret is null");
+		else if(token.equals(System.getProperty("mySecretKey")))
 			System.out.println("valid secret");
 		else
 			System.out.println("invalid secret");
